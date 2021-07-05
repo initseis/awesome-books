ccf3de90a6fe99872bf4a854efc757d54f1f0c81
Kevin <kevinsevinche@gmail.com>
2021-07-05 16:18:08 -0500

    Fix Linter Errors

--- scripts/main.js
+++ scripts/main.js
@@ -15,25 +15,36 @@ if (!localStorage.myStringifyStorage) {
   localStorage.setItem('myStringifyStorage', JSON.stringify(books));
 }
 
+function removeBook(){
+  bookList.removeChild(document.getElementById(`book-${this.id}`));
+  let index = 0;
+  for (let i = 0; i < books.length; i += 1) {
+    if (books[i].id == this.id) {
+      index = i;
+    }
+  }
+  books.splice(index, 1);
+  localStorage.setItem('myStringifyStorage', JSON.stringify(books));
+}
+
 const localStorageBooks = JSON.parse(localStorage.getItem('myStringifyStorage'));
-let id_count = 0;
-console.log(localStorageBooks);
+let idCount = 0;
 for (let i = 0; i < localStorageBooks.length; i += 1) {
   books.push(localStorageBooks[i]);
   const bookLi = document.createElement('li');
   bookLi.id = `book-${localStorageBooks[i].id}`;
   bookList.appendChild(bookLi);
   const bookTitleP = document.createElement('p');
-  bookTitleP.innerHTML= localStorageBooks[i].title;
+  bookTitleP.innerHTML = localStorageBooks[i].title;
   bookLi.appendChild(bookTitleP);
   
   const bookAuthorP = document.createElement('p');
-  bookAuthorP.innerHTML= localStorageBooks[i].author;
+  bookAuthorP.innerHTML = localStorageBooks[i].author;
   bookLi.appendChild(bookAuthorP);
   
   const removeButton = document.createElement('button');
   removeButton.className = 'remove-button';
-  removeButton.innerHTML = "Remove";
+  removeButton.innerHTML = 'Remove';
   removeButton.id = localStorageBooks[i].id;
   bookLi.appendChild(removeButton);
   
@@ -41,49 +52,33 @@ for (let i = 0; i < localStorageBooks.length; i += 1) {
   bookLi.appendChild(hrLi);
   
   removeButton.addEventListener('click', removeBook);
-  if (localStorageBooks[i].id > id_count) {
-    id_count = localStorageBooks[i].id;
+  if (localStorageBooks[i].id > idCount) {
+    idCount = localStorageBooks[i].id;
   }
 }
 
-function addBook(){
-  id_count ++;
-  const newBook = new book(id_count, bookTitle.value, bookAuthor.value);
+function addBook() {
+  idCount += 1;
+  const newBook = new book(idCount, bookTitle.value, bookAuthor.value);
   books.push(newBook);
   const bookLi = document.createElement('li');
   bookLi.id = `book-${newBook.id}`;
   bookList.appendChild(bookLi);
   const bookTitleP = document.createElement('p');
-  bookTitleP.innerHTML= newBook.title;
+  bookTitleP.innerHTML = newBook.title;
   bookLi.appendChild(bookTitleP);
   const bookAuthorP = document.createElement('p');
-  bookAuthorP.innerHTML= newBook.author;
+  bookAuthorP.innerHTML = newBook.author;
   bookLi.appendChild(bookAuthorP);
   const removeButton = document.createElement('button');
   removeButton.className = 'remove-button';
-  removeButton.innerHTML = "Remove";
+  removeButton.innerHTML = 'Remove';
   removeButton.id = newBook.id;
   bookLi.appendChild(removeButton);
   const hrLi = document.createElement('hr');
   bookLi.appendChild(hrLi);
   removeButton.addEventListener('click', removeBook);
   localStorage.setItem('myStringifyStorage', JSON.stringify(books));
-  
 }
 
 submitButton.addEventListener('click', addBook);
-
-function removeBook(){
-  bookList.removeChild(document.getElementById(`book-${this.id}`));
-  
-  let index = 0;
-  for (let i = 0; i < books.length; i++) {
-    if (books[i].id == this.id) {
-      index = i;
-    }
-  }
-  
-  books.splice(index, 1);
-
-  localStorage.setItem('myStringifyStorage', JSON.stringify(books));
-}
