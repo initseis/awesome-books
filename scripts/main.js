4a00c038dd0048b18d71e9103aff8127e692e791
Kevin <kevinsevinche@gmail.com>
2021-07-05 14:04:54 -0500

    Add remove function

--- scripts/main.js
+++ scripts/main.js
@@ -12,13 +12,14 @@ const bookTitle = document.getElementById("book-title");
 const bookAuthor = document.getElementById("book-author");
 const bookList = document.querySelector('.book-list');
 
+
 submitButton.addEventListener('click', addBook);
 
 function addBook(){
     const newBook = new book(books.length+1, bookTitle.value, bookAuthor.value);
     books.push(newBook);
     const bookLi = document.createElement('li');
-    bookLi.id = `book-${books.length}`;
+    bookLi.id = `book-${newBook.id}`;
     bookList.appendChild(bookLi);
     const bookTitleP = document.createElement('p');
     bookTitleP.innerHTML= newBook.title;
@@ -29,12 +30,27 @@ function addBook(){
     bookLi.appendChild(bookAuthorP);
     
     const removeButton = document.createElement('button');
+    removeButton.className = 'remove-button';
     removeButton.innerHTML = "Remove";
+    removeButton.id = books.length;
     bookLi.appendChild(removeButton);
 
     const hrLi = document.createElement('hr');
     bookLi.appendChild(hrLi);
+    
+    removeButton.addEventListener('click', removeBook);
+   
     // console.log(bookTitle.value);
     console.log(books);
 }
 
+// for (let i = 0; i < rmvButtons.length; i++) {
+//   rmvButtons[i].addEventListener('click', removeBook);
+//   console.log("45")
+// }
+
+function removeBook(){
+  // bookList.removeChild(book);
+  bookList.removeChild(document.getElementById(`book-${this.id}`));
+  // alert(this.id);
+}
